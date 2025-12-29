50fe71da4e07201dc0a5bcadc07acb244035e01a
Your Name <you@example.com>
2025-12-29 11:44:29 +0700

    fix: add assetPrefix for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://nice-plant-00469a700.azurestaticapps.net' : undefined,
 };
 
 export default nextConfig;
