3fea15a853104a7f0b804f02fff7bba3b67c1a81
Your Name <you@example.com>
2025-12-29 11:57:05 +0700

    fix: correctly configure Next.js build for Azure SWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://nice-plant-00469a700.azurestaticapps.net' : undefined,
 };
 
 export default nextConfig;
